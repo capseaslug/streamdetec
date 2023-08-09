f640d231acfcb210e353c7454e57e1ca7a5aaff6
capseaslug <todelldesign@gmail.com>
2023-08-08 19:19:45 -0700

    Update sdetect.py

--- sdetect.py
+++ sdetect.py
@@ -1,79 +1,104 @@
-import pcapy
-import re
 import threading
-import tkinter as tk
-from tkinter import scrolledtext
 import psutil
+import pyshark
 import netifaces
+import asyncio
+import curses
+import queue
+import time
+import os
+import logging
+
+# Configure the logging module
+logging.basicConfig(filename='error_log.txt', level=logging.ERROR,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 class PacketAnalyzer:
-    def __init__(self, interface, log_text_widget):
+    def __init__(self, interface, queue, loop):
         self.interface = interface
-        self.cap = pcapy.open_live(interface, 65536, True, 0)
-        self.log_text_widget = log_text_widget
-    
-    def start(self):
-        threading.Thread(target=self._packet_sniffer).start()
-    
-    def _packet_sniffer(self):
-        while True:
-            header, packet = self.cap.next()
-            payload = packet[14:]  # Strip Ethernet header
-            if self._is_video_stream(payload):
-                result = f"Detected potential video stream on interface {self.interface}\n"
-                self.log_text_widget.insert(tk.END, result)
+        self.queue = queue
+        self.capture = pyshark.LiveCapture(interface=self.interface)
+        self.loop = loop  # Pass the existing event loop
 
-    def _is_video_stream(self, payload):
+   def is_video_stream(self, payload):
+        # Traditional Video Patterns
         video_patterns = [
-            rb'\x00\x00\x01\xb0',  # MPEG-2 start code
-            rb'\x00\x00\x00\x01\x67',  # H.264 start code
-            rb'flv\x01',  # FLV header
+            b'\x00\x00\x00\x01',  # MPEG-4 Visual / H.264 / HEVC video formats
+            b'\x00\x00\x01\xB3',  # Motion JPEG 2000 video format
+            b'\x9D\x01\x2A\xDF',  # AVCHD / AVCHD Lite video format
+            b'\x30\x2A\xD7\x30',  # H.263 video format
         ]
-        
-        for pattern in video_patterns:
-            if re.search(pattern, payload):
+
+        # Unorthodox Video Patterns
+        unorthodox_video_methods = [
+            b'\xFF\xD8',           # Start of JPEG image
+            b'\x89\x50\x4E\x47',   # Start of PNG image
+            b'BM',                 # Start of BMP image
+            b'\x1A\x45\xDF\xA3',   # Start of WebM video
+            b'FLV',                # Start of Flash Video (FLV) file
+            b'GIF87a',             # Start of GIF 87a image
+            b'GIF89a',             # Start of GIF 89a image
+            b'\x00\x00\x00\x18',   # Start of 3GPP media file
+            # AVI video format
+            b'RIFF....AVI LIST',
+            # MOV video format
+            b'moov....mdat',
+        ]
+
+        for method in unorthodox_video_methods:
+            if method in payload:
                 return True
+
         return False
 
-def start_analyzer(interface, log_text_widget):
-    analyzer = PacketAnalyzer(interface, log_text_widget)
-    analyzer.start()
+    async def _packet_capture(self):
+        for packet in self.capture.sniff_continuously():
+            payload = packet.raw_mode.packet_data
+            if self._is_video_stream(payload):
+                self.queue.put(f"Detected video stream on {self.interface}")
 
-def check_monitoring_processes(log_text_widget):
-    for process in psutil.process_iter(['pid', 'name', 'cmdline']):
-        try:
-            cmdline = process.info['cmdline']
-            
-            if "keylogger" in ' '.join(cmdline).lower() or "monitoring" in ' '.join(cmdline).lower():
-                result = f"Potential monitoring process detected: {process.info['name']}\n"
-                log_text_widget.insert(tk.END, result)
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
+    def start(self):
+ def start(self):
+        asyncio.set_event_loop(self.loop)  # Use the existing loop
+        asyncio.ensure_future(self._packet_capture())  # Schedule the coroutine
+        self.loop.run_forever()  # Run the loop indefinitely
+# ... (rest of the ProgressBar class)
 
-def start_button_callback(interface_var, log_text_widget):
-    interface = interface_var.get()
-    start_analyzer(interface, log_text_widget)
-    check_monitoring_processes(log_text_widget)
+def main(stdscr):
+    interfaces = netifaces.interfaces()
+    total_interfaces = len(interfaces)  # Including 'lo' interfaces
+    analyzer_threads = []
+    streams_queue = queue.Queue()
+    video_streams = []
 
-# Create GUI window
-root = tk.Tk()
-root.title("Network Analyzer")
+    loop = asyncio.get_event_loop()
+    progress_bar = ProgressBar(total_interfaces, stdscr)
+    progress_thread = threading.Thread(target=progress_bar_update, args=(progress_bar, stdscr))  # Pass stdscr here
+    progress_thread.start()
 
-# Create and place interface selection label and entry
-interface_label = tk.Label(root, text="Interface:")
-interface_label.pack()
+    for interface in interfaces:
+        analyzer = PacketAnalyzer(interface, streams_queue, loop)
+        thread = threading.Thread(target=analyzer.start)
+        thread.start()
+        analyzer_threads.append(thread)
 
-interface_var = tk.StringVar()
-interface_entry = tk.Entry(root, textvariable=interface_var)
-interface_entry.pack()
+    try:
+        curses.wrapper(main)
+    except KeyboardInterrupt:
+        pass
 
-# Create and place start button
-start_button = tk.Button(root, text="Start Analysis", command=lambda: start_button_callback(interface_var, log_text))
-start_button.pack()
+def progress_bar_update(progress_bar, stdscr):
+    while True:
+        if not streams_queue.empty():
+            stream = streams_queue.get()
+            video_streams.append(stream)
+        row = 0
+        for stream in video_streams:
+            stdscr.addstr(row, 0, stream)
+            row += 1
+        stdscr.refresh()
+        curses.napms(100)  # Sleep for 100 milliseconds
+        progress_bar.update()
 
-# Create and place log text widget
-log_text = scrolledtext.ScrolledText(root, wrap=tk.WORD)
-log_text.pack()
-
-# Run GUI main loop
-root.mainloop()
+if __name__ == "__main__":
+    curses.wrapper(main)
